f63742d460a62af281bb5c70be2d6d7d6e78a4ff
sumit021995 <kaku.saurav@gmail.com>
2023-11-10 23:43:44 +0530

    Some event basis

--- project1/colorchanger.js
+++ project1/colorchanger.js
@@ -5,8 +5,12 @@
     console.log(button);
     button.addEventListener('click',(event)=>{
         console.log(event);
-        console.log(event.target);
+        // console.log(event.target);
+        
         
 
     })
  })
+
+ // Some event basis
+ // like 
