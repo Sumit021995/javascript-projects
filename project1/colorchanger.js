921807d9c84277547b1bad6d42118dc2401007e7
sumit021995 <kaku.saurav@gmail.com>
2023-11-11 22:47:50 +0530

    project 1 continue

--- project1/colorchanger.js
+++ project1/colorchanger.js
@@ -1,18 +1,32 @@
  const buttons = document.querySelectorAll('.button');
  const body = document.querySelector('body');
 
- buttons.forEach((button)=>{
-    console.log(button);
-    button.addEventListener('click',(event)=>{
-        console.log(event);
-        // console.log(event.target);
-        
-        
-
-    })
- })
-
  // Some event basis
  // like movement of cursor is an event
  // click on anything is also an event
  // Tab closing is also an event
+ console.log(buttons);
+ /*
+ NodeList(4) [span#gray.button, span#white.button, span#blue.button, span#yellow.button]
+0: span#gray.button
+1: span#white.button
+2: span#blue.button
+3: span#yellow.button
+length: 4
+[[Prototype]]: NodeList
+  */
+ 
+
+ buttons.forEach((button)=>{
+    console.log(button);
+
+    button.addEventListener('click',(event)=>{
+        console.log(event);
+        console.log(event.target);
+        
+        
+
+    })
+ });
+
+ 
