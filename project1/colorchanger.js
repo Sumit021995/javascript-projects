f013e15703c65899b99fcdff4f516b6362788d3b
sumit021995 <kaku.saurav@gmail.com>
2023-11-29 11:46:16 +0530

    commenting all console.logs

--- project1/colorchanger.js
+++ project1/colorchanger.js
@@ -5,7 +5,7 @@
  // like movement of cursor is an event
  // click on anything is also an event
  // Tab closing is also an event
- console.log(buttons);
+ //.........console.log(buttons);
  /*
  NodeList(4) [span#gray.button, span#white.button, span#blue.button, span#yellow.button]
 0: span#gray.button
@@ -18,17 +18,17 @@ length: 4
  
 
  buttons.forEach((button)=>{
-    console.log(button);
+    //.........console.log(button);
     //span#gray.button
     //span#white.button
     //span#blue.button
     //span#yellow.button
 
     button.addEventListener('click', function(element){
-        console.log(element);
+        //.........console.log(element);
         // when we click on any button with color given then console.log(element) shows
         // PointerEvent {isTrusted: true, pointerId: 3, width: 1, height: 1, pressure: 0, …}
-        console.log(element.target);
+        //.........console.log(element.target);
         // in PointerEvent {} .. there is a target 
         // in 'target' there is an 'id' which relate to id in index.html file
         // and that id relate to id of different colors where 'addEventListner' is working 
