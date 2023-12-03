b439073daab6ebe532edef6ae7d81f421722f5cb
sumit021995 <kaku.saurav@gmail.com>
2023-12-03 23:26:42 +0530

    project2 updated

--- BMI calculator/bmiCalculator.js
+++ BMI calculator/bmiCalculator.js
@@ -2,9 +2,9 @@ let form = document.querySelector('form');
 
 form.eventListener('submit', (s)=>{
     s.preventDefault();
-    let height = parseInt(document.querySelector('#height'));
-    let weight = parseInt(document.querySelector('#weight'));
-    let result = parseInt(document.querySelector('#result')); 
+    let height = parseInt(document.querySelector('#height').value);
+    let weight = parseInt(document.querySelector('#weight').value);
+    let result = parseInt(document.querySelector('#result').value); 
 
     result = (weight/((height/100)^2));
 
