05e15adf53511bc90386f482fcc2999551f6e46b
sumit021995 <kaku.saurav@gmail.com>
2023-12-04 13:25:52 +0530

    project2 updated

--- BMI calculator/bmiCalculator.js
+++ BMI calculator/bmiCalculator.js
@@ -1,12 +1,14 @@
-const form = document.querySelector('form'); 
+const form = document.querySelector('form');
+// const height = parseInt(document.querySelector('#height').value);
+// this use case will give you empty result as no entry is there before submission
 
-form.addEventListener('submit', (s)=>{
-    s.preventDefault();
-    const height = parseInt(document.querySelector('#height').value);
-    const weight = parseInt(document.querySelector('#weight').value);
-    const result = parseInt(document.querySelector('#result')); 
+form.addEventListener('submit', function (s) {
+  s.preventDefault();
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
 
-    result = (weight/((height/100)^2)); 
-
-
-}) 
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = 'Please give a valid height';
+  }
+});
