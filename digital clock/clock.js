a5852929b0f5c3615092658498cb1dd7869ced92
sumit021995 <kaku.saurav@gmail.com>
2023-12-05 14:55:26 +0530

    project3 updated

---
+++ digital clock/clock.js
@@ -0,0 +1,5 @@
+let clock = document.getElementById('clock');
+let date = new Date();
+date = date.toLocaleTimeString();
+// console.log(date.toLocaleTimeString());
+// 2:52:59 pm it shows the current time which is changing 
