c8ba50f4048942f25ab52f32f39766d794a6cba6
sumit021995 <kaku.saurav@gmail.com>
2023-12-07 23:39:26 +0530

    project4 updated

--- guess the number/guessnumber.js
+++ guess the number/guessnumber.js
@@ -2,27 +2,27 @@ let form = document.querySelector('.form');
 let submit = document.getElementById('subt');
 
 form.addEventListener('submit',function(){
-    let guessField = parseInt(document.getElementById('guessField').value);
+    let userInput = parseInt(document.getElementById('guessField').value);
     let guesses = document.querySelector('.guesses');
     let lastResult = document.querySelector('.lastResult');
     let randomNumber = ((Math.random()*100) + 1);
     
     //this is a button as you can seen on live server from index.html
     // let guessSubmit = document.querySelector('.guessSubmit');
-    if(guessField === randomNumber){
+    if(userInput === randomNumber){
         let lowOrHi = document.querySelector('.lowOrHi');
         lowOrHi.innerHTML = 'Your guess is correct';
         
-    } else if(guessField > randomNumber){
+    } else if(userInput > randomNumber){
         lowOrHi.innerHTML = 'Your guess is greater than Random Number';
 
     }
-     else if(guessField < randomNumber){
+     else if(userInput < randomNumber){
         lowOrHi.innerHTML = 'Your guess is less  than Random Number';
 
     }
 
 
 
-
+ 
 });
