64d407b7c4f1b196b98e12fda6407ad6ffce29c3
sumit021995 <kaku.saurav@gmail.com>
2023-12-07 10:53:01 +0530

    project 4 updated

--- guess the number/guessnumber.js
+++ guess the number/guessnumber.js
@@ -1,6 +1,8 @@
 let form = document.querySelector('.form');
 
 form.addEventListener('submit',function(){
-    let guessFiels = document.getElementById('guessField');
-    let randomNumber = ((Math.random()*100) + 1)
-})
+    let guessFiels = parseInt(document.getElementById('guessField').value);
+    let guesses = document.querySelector('.guesses');
+    let lastResult = document.querySelector('.lastResult');
+    let randomNumber = ((Math.random()*100) + 1);
+});
