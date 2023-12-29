fe800881942edbc4d747242a371596d17011097e
sumit021995 <kaku.saurav@gmail.com>
2023-12-29 14:35:41 +0530

    async code study project

---
+++ keyboard/keyboard.js
@@ -0,0 +1,27 @@
+let insert = document.getElementById('insert');
+// here we use an event - keydown event
+// we need a table
+// we need to add an addEventListener to window
+
+/*
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+*/
+
+
+
