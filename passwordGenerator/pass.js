78bb35b35342db92b2a8f1b288603c45cc5935bd
sumit021995 <kaku.saurav@gmail.com>
2024-01-17 22:49:23 +0530

    FILE UPDATED

--- passwordGenerator/pass.js
+++ passwordGenerator/pass.js
@@ -1,2 +1,7 @@
 // You have been asked to create a random password generator. The password generator must ask the user for the desired length of the password (minimum four characters). Each password must have at least one digit excluding zero and one (2 - 9) at least one lowercase letter other than a lower case L, at least one uppercase letter other than O and at least one of these characters between the brackets (!@#$%^&*). What steps would you follow to produce a valid password?
 
+let password = ()=>{
+    let alfa = 'qwertyuiopasdfghjkzxcvbnm'
+    let alfaC = 'QWERTYUIPASDFGHJKLZXCVBNM'
+    
+}
